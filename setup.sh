d1f73dd6f16f040b98231550893400f55aa2c6fe
Grinspo0n <40165801+Grinspo0n@users.noreply.github.com>
2024-10-18 07:58:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -159,7 +159,7 @@ rm -rf "$TEMP_DIR"
 
 #SDDM setup
 sudo systemctl enable sddm
-
+hyprpm update -s
 chsh -s $(which zsh)
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
